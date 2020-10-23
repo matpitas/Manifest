db8662c1c54600df6cbef2e96d9d43e541b4304d
Matheus Pitas <63295491+matpitas@users.noreply.github.com>
2020-10-22 22:42:47 -0300

    Ultimo commit

--- TarefaDDW/sw.js
+++ TarefaDDW/sw.js
@@ -1,27 +1,20 @@
-var CACHE_NAME = 'cache_site';
-var urlsToCache = [
-    'css/bootstrap.css',
-    'js/bootstrap.js',
-    'js/jquery-3.5.1.js'
+var cacheName = 'NomeApp-v';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
 ];
-
-self.addEventListener('install', function(event){
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache){
-            console.log('cache aberto')
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
-
-
-self.addEventListener('fetch',function(event){
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            if(response){
-                return response;
-            }
-            return fetch(event.request)
-        })
-    )
-})
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
